4ba45e2766d609bbb18c32804cd1592eb19d11f7
Arun Kumar <112628113+Akgoldie@users.noreply.github.com>
2023-07-16 12:48:31 +0530

    Add files via upload

---
+++ Task-2 Divisible by 3 or not.py
@@ -0,0 +1,8 @@
+#Number is Divisible by 3 or not
+
+Number = int(input("Please insert number: "))
+
+if (Number%3==0):
+    print(Number,"is divisible by 3")
+else:
+    print(Number,"is not divisible by 3")
---
+++ Task-2 Largest of two numbers.py
@@ -0,0 +1,9 @@
+#which is largest of two numbers
+
+num1 = int(input("Enter the number 1 :"))
+num2 = int(input("Enter the number 2 :"))
+
+if (num1 > num2):
+    print(num1,"is greatest")
+else:
+    print(num2,"is greatest")
